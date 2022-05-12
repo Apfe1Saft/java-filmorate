5bc9fbaa13a9e12891abb0ebb33595021d94bdfc
Apfe1Saft <foraaivanov@gmail.com>
2022-05-12 12:48:47 +0300

    update UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -46,29 +46,33 @@ public class UserController {
             log.error("ОШИБКА /POST : Не правильно введен пользователь");
             throw new ValidationException("Не правильно введен пользователь");
         }catch (Exception exception){
-            System.out.println("ERROR");
             response.setStatus(400);
         }
     }
 
     @PatchMapping
-    public void update(@RequestBody String body) {
-        Gson gson = StaticData.gson;
-        User user = gson.fromJson(body,User.class);
-        if (!user.getEmail().equals("") && user.getEmail().contains("@")) {
-            if (!user.getLogin().equals("") && !user.getLogin().contains(" ")) {
-                if (user.getName().equals("")) {
-                    user.setName(user.getLogin());
-                }
-                if (user.getBirthday().isBefore(LocalDate.now())) {
-                    users.removeIf(u -> u.getId() == user.getId());
-                    users.add(user);
-                    log.info("/PATCH обновлен пользователь");
-                    return;
+    public void update(@RequestBody String body, HttpServletResponse response) {
+        try {
+            Gson gson = StaticData.gson;
+            User user = gson.fromJson(body, User.class);
+            if (!user.getEmail().equals("") && user.getEmail().contains("@")) {
+                if (!user.getLogin().equals("") && !user.getLogin().contains(" ")) {
+                    if (user.getName().equals("")) {
+                        user.setName(user.getLogin());
+                    }
+                    if (user.getBirthday().isBefore(LocalDate.now())) {
+                        users.removeIf(u -> u.getId() == user.getId());
+                        users.add(user);
+                        log.info("/PATCH обновлен пользователь");
+                        response.setStatus(200);
+                        return;
+                    }
                 }
             }
+            log.error("ОШИБКА /PATCH : Не правильно введен пользователь");
+            throw new ValidationException("Не правильно введен пользователь");
+        }catch (Exception exception){
+            response.setStatus(400);
         }
-        log.error("ОШИБКА /PATCH : Не правильно введен пользователь");
-        throw new ValidationException("Не правильно введен пользователь");
     }
 }
